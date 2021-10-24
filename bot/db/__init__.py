404275d36a5a4301b5f96fc83ac56cf57e65ade7
SJH <ikswary@naver.com>
2021-10-24 18:27:16 +0900

    feat : import added models

--- bot/db/__init__.py
+++ bot/db/__init__.py
@@ -1,17 +1,26 @@
-import bot.db.base
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker, Session
 
-from bot.db.models.user import *
+import bot.db.base
 from bot.db.models.character import *
+from bot.db.models.content.content import *
+from bot.db.models.content.abyss_dungeon import *
+from bot.db.models.content.abyss_raid import *
+from bot.db.models.content.challenge_abyss_dungeon import *
+from bot.db.models.content.challenge_guardian import *
+from bot.db.models.content.content import *
+from bot.db.models.content.expedition_raid import *
+from bot.db.models.content.raid import *
+from bot.db.models.user import *
+
 
 
 def create_session(url: str) -> Session:
     engine = create_engine(url)
     base.Base.metadata.create_all(engine, checkfirst=True)
     return scoped_session(sessionmaker(autocommit=False,
-                                autoflush=True,
-                                bind=engine))
+                                       autoflush=True,
+                                       bind=engine))
+
 
 instance: Session = None
