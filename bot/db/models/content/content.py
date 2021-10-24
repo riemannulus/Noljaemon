f8229224a51a357375f64ac5d30c4c4f237e4444
SJH <ikswary@naver.com>
2021-10-24 18:19:54 +0900

    feat : add ContentType Enum Class

--- bot/db/models/content/content.py
+++ bot/db/models/content/content.py
@@ -1,5 +1,14 @@
-from sqlalchemy import Column, Integer, String
+from enum import Enum
 from bot.db.base import Base
+
+
+class ContentType(Enum):
+    RAID = 0,
+    EXPEDITION_RAID = 1,
+    ABYSS_RAID = 2,
+    ABYSS_DUNGEON = 3,
+    CHALLENGE_ABYSS_DUNGEON = 4,
+    CHALLENGE_GUARDIAN = 5
 
 
 class Content(Base):
